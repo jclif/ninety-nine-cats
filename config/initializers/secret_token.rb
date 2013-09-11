1ff6479d3796db2ea05fc7b393ac75e12279b225
Jared Clifton / Sarah Quigley <jaredcraigclifton@gmail.com / sarah@sarahquigley.net>
2013-09-11 09:20:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinetyNineCats::Application.config.secret_token = '429ea635faaf1899c48dada82ac89c370b4e6732a79df281ee1533d54444b4cf67ca7529de77b2d0d9fa164d52f27673f2c268ab498b6958ac3f5cecf7a05638'
